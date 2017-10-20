104ba1b6c87c3a6384aff63b26e9391fff000c00
Ryan <ryanfurtado.elre@gmail.com>
2017-10-20 10:38:24 -0400

    File Update - Revised JS + New Solution
    Added the new code from the StackOverflow solution and started cleaning up the new JS file (tmo_card_placement.js) which will be renamed once complete.
    
    Also in case you want to see the changes the files that go together are:
    - tmo_update.html
    - tmo_card_placement.js
    - sibs.js
    
    Enjoy! :smile:

---
+++ TMO - Game/js/sibs.js
@@ -0,0 +1,50 @@
+var siblings_of = [];
+
+siblings_of[1] = [2, 5, 6];
+siblings_of[2] = [1, 3, 5, 6, 7];
+siblings_of[3] = [2, 4, 6, 7, 8];
+siblings_of[4] = [3, 7, 8];
+
+siblings_of[5] = [1, 2, 6, 9, 10];
+siblings_of[6] = [1, 2, 3, 5, 7, 9, 10, 11];
+siblings_of[7] = [2, 3, 4, 6, 8, 10, 11, 12];
+siblings_of[8] = [3, 4, 7, 11, 12];
+
+siblings_of[9] = [5, 6, 10, 13, 14];
+siblings_of[10] = [5, 6, 7, 9, 11, 13, 14, 15];
+siblings_of[11] = [6, 7, 8, 10, 12, 14, 15, 16];
+siblings_of[12] = [7, 8, 11, 15, 16];
+
+siblings_of[13] = [9, 10, 14];
+siblings_of[14] = [9, 10, 11, 13, 15];
+siblings_of[15] = [10, 11, 12, 14, 16];
+siblings_of[16] = [11, 12, 15];
+
+/*
+function mark_siblings(id) {
+  siblings_of[id].forEach(function(item) {
+    $('#' + item).addClass('marked');
+  });
+}
+*/
+
+/*
+function reset() {
+  $('.selected').removeClass('selected');
+  $('.marked').removeClass('marked');
+}
+*/
+
+function identify_siblings(id){
+    console.log("Cards around CPU's Card: " + siblings_of[id]);
+}
+
+
+$(document).ready(function() {
+  $('.panel').on('click', function() {
+    var id = $(this).attr('id');
+    reset();
+    $(this).addClass('selected');
+    mark_siblings(id);
+  })
+});
